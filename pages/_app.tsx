b04c75f2082506c9ea1b00dfb60d81ffaa76eee8
jclin22009 <jclin2.2009@gmail.com>
2023-08-17 23:32:09 -0700

    force darkmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,13 @@ import { ChakraProvider, DarkMode, extendTheme } from '@chakra-ui/react';
 import { Lora } from '@next/font/google';
 import type { AppProps } from 'next/app';
 
-// const lora = Lora({ subsets: ['latin'] });
-
-// const theme = extendTheme({
-//   fonts: {
-//     heading: lora.style.fontFamily
-//   }
-// });
+const theme = extendTheme({
+  // force dark theme
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false
+  }
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
