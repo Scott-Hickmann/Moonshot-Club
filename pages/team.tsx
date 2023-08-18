736d6f180021747eaa89ac595ca739cc3ed09f6b
jclin22009 <jclin2.2009@gmail.com>
2023-08-17 23:43:31 -0700

    our mission and team temp

--- pages/team.tsx
+++ pages/team.tsx
@@ -1,5 +1,24 @@
+import { Box, Heading, Text } from '@chakra-ui/react';
 import Navigation from '../components/navigation';
 
 export default function Team() {
-  return <Navigation />;
+  return (
+    <>
+      <Navigation />
+      <Box
+        id="mission"
+        w={{ base: '100%', md: '60%' }}
+        mx="auto"
+        maxW="1000px"
+        p={5}
+        my={5}
+        bgColor="gray.700"
+        borderRadius="10px"
+        boxShadow="dark-sm"
+      >
+        <Heading pb={5}>Our Team</Heading>
+        <Text>Coming soon!</Text>
+      </Box>
+    </>
+  );
 }
