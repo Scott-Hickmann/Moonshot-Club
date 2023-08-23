0448484fb08ce69f83397543a8758974b9d95620
Andy Tang <winterwind2022@gmail.com>
2023-08-23 08:53:50 -0500

    fix webfonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,26 @@
 import '../styles/global.css';
 
 import { ChakraProvider, DarkMode, extendTheme } from '@chakra-ui/react';
-import { Lora } from '@next/font/google';
+import { Lora } from 'next/font/google';
 import type { AppProps } from 'next/app';
+
+const lora = Lora({ subsets: ['latin'], display: 'swap' });
 
 const theme = extendTheme({
   // force dark theme
   config: {
     initialColorMode: 'dark',
-    useSystemColorMode: false
+    useSystemColorMode: false,
+  },
+  fonts: {
+    heading: lora.style.fontFamily,
+    body: lora.style.fontFamily,
   }
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS /*theme={theme}*/>
+    <ChakraProvider resetCSS theme={theme}>
       <DarkMode>
         <Component {...pageProps} />
       </DarkMode>
