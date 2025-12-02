f176cc20c47deb038c4a30b58197b74bba2ccaba
Jason Lin <jclin2.2009@gmail.com>
2025-12-02 10:25:15 -0800

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/global.css';
 import { ChakraProvider, DarkMode } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
+//test
 import { theme } from '../components/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
